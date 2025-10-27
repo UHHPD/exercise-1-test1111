dded0092141119e829cf9111eadfba2d419665e1
Hartmut Stadie <hartmut.stadie@uni-hamburg.de>
2025-10-27 08:57:01 +0000

    test2

--- summieren.cc
+++ summieren.cc
@@ -1,31 +1,17 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 int main() {
-    std::ifstream file("daten.txt");
-    
-    if (!file.is_open()) {
-        std::cerr << "Fehler beim Öffnen der Datei daten.txt" << std::endl;
-        return 1;
-    }
-    
-    int num1, num2;
-    int summe = 0;
-    int anzahl = 0;
-    
-    while (file >> num1 >> num2) {
-        summe += (num1 + num2);
-        anzahl++;
-    }
-    
-    file.close();
-    
-    std::cout << "Anzahl der Paare: " << anzahl << std::endl;
-    std::cout << "Gesamtsumme: " << summe << std::endl;
-    
-    if (anzahl > 0) {
-        std::cout << "Durchschnitt: " << static_cast<double>(summe) / anzahl << std::endl;
-    }
-    
-    return 0;
+  std::ifstream file("daten.txt");
+  std::ofstream fout("datensumme.txt");
+  int a, b;
+  for (int i = 0; i < 234; ++i) {
+    file >> a >> b;
+    int summe = a + b;
+    fout << summe << "\n";
+  }
+
+  file.close();
+  fout.close();
+  return 0;
 }
